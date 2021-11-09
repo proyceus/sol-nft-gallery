4ab5ddbd00a0c91dc00d68a63ea00071667332df
proyceus <proyceus@gmail.com>
2021-11-09 15:44:32 -0500

    finished with frontend functions

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const TEST_GIFS = [
-  'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
-	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
-	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
-	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+  'https://media.giphy.com/media/3o7qE2VAxuXWeyvJIY/giphy.gif',
+	'https://media.giphy.com/media/DhstvI3zZ598Nb1rFf/giphy.gif',
+	'https://media.giphy.com/media/mFTIkcmQWTbNFrTp2O/giphy.gif',
+	'https://media.giphy.com/media/F9hQLAVhWnL56/giphy.gif',
+  'https://media.giphy.com/media/w7mLEAMcpjrpe/giphy.gif',
+  'https://media.giphy.com/media/tsX3YMWYzDPjAARfeg/giphy.gif'
 ]
 
 const App = () => {
 
   const [walletAddress, setWalletAddress] = useState(null);
   const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
 
   //checks to see if user has Phantom wallet
 
@@ -46,10 +49,23 @@ const App = () => {
   }
 
   useEffect(() => {
-    window.addEventListener('load', async (event) => {
+    const onLoad = async () => {
       await checkIfWalletIsConnected();
-    });
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
   }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIF list...');
+
+      //Call Solana Program here
+
+      //Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
 
   const connectWallet = async () => {
     const { solana } = window;
@@ -58,6 +74,14 @@ const App = () => {
       const response = await solana.connect();
       console.log('Connected with Public Key: ', response.publicKey.toString());
       setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log('Gif link:', inputValue);
+    } else {
+      console.log('Empty input. Try again.');
     }
   };
 
@@ -73,9 +97,9 @@ const App = () => {
   const renderConnectedContainer = () => (
     <div className="connected-container">
       <input type="text" placeholder="Enter gif link!" value={inputValue} onChange={onInputChange} />
-      <button className="cta-button submit-gif-button">Submit</button>
+      <button className="cta-button submit-gif-button" onClick={sendGif}>Submit</button>
       <div className="gif-grid">
-        {TEST_GIFS.map(gif => (
+        {gifList.map(gif => (
           <div className="gif-item" key={gif}>
             <img src={gif} alt={gif} />
           </div>
@@ -89,9 +113,9 @@ const App = () => {
     <div className="App">
       <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
-          <p className="header">NFT Gallery</p>
+          <p className="header">GIF Dance Party</p>
           <p className="sub-text">
-            Customize your own NFT Gallery
+            Add your favorite dancing GIFs!
           </p>
           {!walletAddress ? renderNotConnectedContainer() : renderConnectedContainer()}
         </div>
