9b59d38bd996710542032bf652b3f4402232b714
proyceus <proyceus@gmail.com>
2021-11-09 14:58:57 -0500

    adding in GIF functions that will change later to NFT gallery

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import { useEffect, useState } from 'react';
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+const TEST_GIFS = [
+  'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+]
+
 const App = () => {
 
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
 
   //checks to see if user has Phantom wallet
 
@@ -53,8 +61,27 @@ const App = () => {
     }
   };
 
+  const onInputChange = (e) => {
+    const { value } = e.target;
+    setInputValue(value);
+  };
+
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button" onClick={connectWallet}>Connect to wallet</button>
+  )
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <input type="text" placeholder="Enter gif link!" value={inputValue} onChange={onInputChange} />
+      <button className="cta-button submit-gif-button">Submit</button>
+      <div className="gif-grid">
+        {TEST_GIFS.map(gif => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 
 
@@ -66,7 +93,7 @@ const App = () => {
           <p className="sub-text">
             Customize your own NFT Gallery
           </p>
-          {!walletAddress && renderNotConnectedContainer()}
+          {!walletAddress ? renderNotConnectedContainer() : renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
